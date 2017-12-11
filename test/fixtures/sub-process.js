505df9b6f6e83504bdc4eed05749e057d9dfa8ba
Sunhuizhe <674277449@qq.com>
2017-12-11 14:27:00 +0800

    add some fileses

---
+++ test/fixtures/sub-process.js
@@ -0,0 +1,16 @@
+'use strict';
+const Insight = require('../../lib');
+
+const insight = new Insight({
+	packageName: 'yeoman',
+	packageVersion: '0.0.0',
+	trackingCode: 'GA-1234567-1'
+});
+
+if (process.env.permissionTimeout) {
+	insight._permissionTimeout = process.env.permissionTimeout;
+}
+
+insight.askPermission('', () => {
+	process.exit(145);
+});
